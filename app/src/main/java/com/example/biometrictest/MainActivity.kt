c89fc6ffd28084ab669bad200ee80ae8f002a679
ankitbajaj1989 <ankitbajaj1989@gmail.com>
2020-04-22 01:17:08 +0530

    Gradle plugin version

--- app/src/main/java/com/example/biometrictest/MainActivity.kt
+++ app/src/main/java/com/example/biometrictest/MainActivity.kt
@@ -2,11 +2,87 @@ package com.example.biometrictest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+import androidx.core.hardware.fingerprint.FingerprintManagerCompat
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.concurrent.Executor
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var executor: Executor
+    private lateinit var biometricPrompt: BiometricPrompt
+    private lateinit var promptInfo: androidx.biometric.BiometricPrompt.PromptInfo
+    private lateinit var biometricManager: BiometricManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        executor = ContextCompat.getMainExecutor(this)
+
+
+        biometricManager = BiometricManager.from(this)
+        checkBioMetricStatus(biometricManager)
+
+        biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback() {
+                override fun onAuthenticationError(
+                    errorCode: Int,
+                    errString: CharSequence
+                ) {
+                    super.onAuthenticationError(errorCode, errString)
+                    Log.i("onAuthenticationError", "Error Code $errorCode ErrorString $errString")
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication error: $errString", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationSucceeded(
+                    result: BiometricPrompt.AuthenticationResult
+                ) {
+                    super.onAuthenticationSucceeded(result)
+                    Toast.makeText(
+                        applicationContext,
+                        "Authentication succeeded!", Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    Toast.makeText(
+                        applicationContext, "Authentication failed",
+                        Toast.LENGTH_SHORT
+                    )
+                        .show()
+                }
+            })
+
+
+        promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("Biometric login for my app")
+            .setSubtitle("Log in using your biometric credential")
+            .setNegativeButtonText("Cancel")
+            .build()
+
+        biometric.setOnClickListener {
+            biometricPrompt.authenticate(promptInfo)
+        }
+
+    }
+
+    fun checkBioMetricStatus(biometricManager: BiometricManager) {
+        when (biometricManager.canAuthenticate()) {
+            BiometricManager.BIOMETRIC_SUCCESS -> Log.i("Biometric status", "App can use biometric")
+            BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE -> Log.i("Biometric status", "Biometric harware problem")
+            BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> Log.i("Biometric status", "Biometric feature currently unavailable")
+            BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> Log.i("Biometric status", "Biometric not Enrolled")
+        }
     }
 }
